90328c4d695511e98208b584805b5fe39921f57b
Max Lepin <max.09@outlook.com>
2019-12-19 14:07:15 +0100

    update gitgnore

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
-
 from helpers import *
 import datetime
 import requests
