ff9eaa8dcb1c0c8f6c91b04ff0c2dd5950314e14
Max Lepin <max.09@outlook.com>
2019-12-20 11:13:00 +0100

    add /leagues route and html page

--- app.py
+++ app.py
@@ -181,10 +181,8 @@ def login():
 @app.route("/logout")
 def logout():
     """Log user out"""
-
     # Forget any user_id
     session.clear()
-
     # Redirect user to login form
     return redirect("/")
 
